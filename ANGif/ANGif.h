cf49bb5700c87782913392c25c6c2129ba7b97d1
Mike <ccteym@gmail.com>
2015-08-05 21:01:35 +0800

    ignore

---
+++ ANGif/ANGif.h
@@ -0,0 +1,19 @@
+//
+//  ANGif.h
+//  ANGif
+//
+//  Created by WangMike on 15/6/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ANGif.
+FOUNDATION_EXPORT double ANGifVersionNumber;
+
+//! Project version string for ANGif.
+FOUNDATION_EXPORT const unsigned char ANGifVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANGif/PublicHeader.h>
+
+
